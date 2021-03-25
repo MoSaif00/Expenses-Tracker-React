bdd854f44b37f4f35a25d5a7ad2108855364cdeb
MoSaif00 <moh14ab@gmail.com>
2021-03-25 04:50:14 +0100

    finished code along and added some styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React from 'react';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
+import {GlobalProvider} from './context/GlobalState';
+
 import './App.css';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <AddTransaction />
+        <IncomeExpenses />
+
+        <Balance />
+        <TransactionList />
+      </div>
+    </GlobalProvider>
+  );
 }
 
 export default App;
---
+++ src/components/Transaction.js
@@ -0,0 +1,26 @@
+import React, {useContext} from 'react';
+import {GlobalContext} from '../context/GlobalState';
+
+const Transaction = (props) => {
+  const {transaction} = props;
+  const {deleteTransaction} = useContext(GlobalContext);
+
+  const sign = transaction.amount < 0 ? '-' : '+';
+
+  return (
+    <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+      {transaction.text}{' '}
+      <span>
+        {sign}€{Math.abs(transaction.amount)}
+      </span>
+      <button
+        className="delete-btn"
+        onClick={() => deleteTransaction(transaction.id)}
+      >
+        x
+      </button>
+    </li>
+  );
+};
+
+export default Transaction;
